54aec2401ffe574494edfd9ecfb6e1190f54408a
Pratik Majee <pratickmajee@gmail.com>
2025-07-27 02:45:54 +0530

    fully upgraded to 0.80.2

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import SignUp from './src/screens/auth/SignUp';
+
 import NavigationHandler from './src/navigation/NavigationHandler';
-import Home from './src/screens/main/Home';
+
 
 const App = () => {
   return <NavigationHandler />;
